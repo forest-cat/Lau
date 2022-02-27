34b44e97e9c6625c6fc636ea9e99493a94d8f077
forest-cat <forest_cat@dismail.de>
2022-02-27 15:35:37 +0100

    Added some request stuff in r6tracker.py

--- cogs/r6tracker.py
+++ cogs/r6tracker.py
@@ -1,4 +1,5 @@
 from discord.commands import slash_command, Option 
+from requests_html import AsyncHTMLSession
 from discord.ext import commands
 import discord
 import json
@@ -15,11 +16,19 @@ class R6Tracker(commands.Cog):
     with open("../config.json", "r") as config:
         config = json.load(config)
 
+    async def get(self):
+        session = AsyncHTMLSession()
+        page1 = await session.get("https://r6.tracker.network/profile/pc/BurakDasBoereck")
+        await page1.html.arender(timeout=6000)
+        print("531" in page1.text)
+
     # https://r6.tracker.network/profile/pc/BurakDasBoereck
     @slash_command(guild_ids=config["test_guild_id"])
-    async def stats(self, ctx, device: Option(str, "Choose the Device", choices=["PC", "PlayStation", "XBox"]), username: str):
+    async def stats(self, ctx, device: Option(str, "Choose a Device", choices=["PC", "PlayStation", "XBox", "Toaster"]), username: str):
         """Says hello from the Cog"""
         await ctx.respond(f"{username} is playing on {device}")
+        await self.get()
+        
         
 
 def setup(bot):
